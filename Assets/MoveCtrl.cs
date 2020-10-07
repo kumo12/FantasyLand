d49cadd173290363b0dbb70f496f55a02363a18a
kumo12 <63216449+kumo12@users.noreply.github.com>
2020-10-07 11:15:59 +0900

    .

---
+++ Assets/MoveCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour
+{
+    public Transform[] points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject wayPointGroup = GameObject.Find("WayPointGroup");
+        if (wayPointGroup != null)
+        {
+            points = wayPointGroup.GetComponentsInChildren<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
